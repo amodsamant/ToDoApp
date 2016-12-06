d7e93b34aa62be017fe5cdfd6cc05f96f1ab55de
amodsamant <samant.amod@gmail.com>
2016-12-05 23:19:59 -0800

    Added add and long click remove

--- app/src/main/java/com/todoapp/MainToDoActivity.java
+++ app/src/main/java/com/todoapp/MainToDoActivity.java
@@ -1,13 +1,57 @@
 package com.todoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainToDoActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_to_do);
+
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First thing");
+        items.add("Second thing");
+
+        setupListViewListener();
     }
+
+    public void onAddItem(View v) {
+
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+    private void setupListViewListener() {
+
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View item, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
 }
